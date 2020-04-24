295a66acebc09b29b4b3693d9077eb52ebbf0dfa
Axel Huebl <axel.huebl@plasma.ninja>
2020-04-24 09:53:00 -0700

    hdf5 conflict with MPI
    Submitted as separate PR since CI in #53606 cannot add a Formula
    and modify another one at the same time for some reason.

--- Formula/hdf5.rb
+++ Formula/hdf5.rb
@@ -19,6 +19,8 @@ class Hdf5 < Formula
 
   uses_from_macos "zlib"
 
+  conflicts_with "hdf5-mpi", :because => "hdf5-mpi is a variant of hdf5, one can only use one or the other"
+
   def install
     inreplace %w[c++/src/h5c++.in fortran/src/h5fc.in bin/h5cc.in],
       "${libdir}/libhdf5.settings",
